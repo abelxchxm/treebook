e2b2de0c5dcebfc5c3244cd3d7461dea60eedf6c
Abel Chavez <abelxchxm@gmail.com>
2015-03-16 17:15:57 -0600

    Styled the flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+    when :alert.to_s
+      "alert-error"
+    when :notice.to_s
+      "alert-success"
+    else
+      ""
+    end
+  end
 end
